477dd7d6b138f491829b58072bb322b374af0c18
Fukuemon <Fukuemony029@gmail.com>
2024-10-15 13:34:10 +0900

    ✨ feat: repositoryにエラーハンドリングを追加 #6

--- internal/infrastructure/mysql/repository/position_repository.go
+++ internal/infrastructure/mysql/repository/position_repository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	errorDomain "api-buddy/domain/error"
 	positionDomain "api-buddy/domain/facility/position"
 	"api-buddy/infrastructure/mysql/db"
 	"context"
@@ -19,24 +20,33 @@ func NewPositionRepository() positionDomain.PositionRepository {
 }
 
 func (r *PositionRepository) Create(ctx context.Context, position *positionDomain.Position) error {
-	if err := r.db.Create(position).Error; err != nil {
-		return err
+	err := r.db.Create(position).Error
+	if err != nil {
+		return errorDomain.WrapError(errorDomain.GeneralDBError, err)
 	}
 	return nil
 }
 
 func (r *PositionRepository) FindByID(ctx context.Context, id string) (*positionDomain.Position, error) {
 	var position positionDomain.Position
-	if err := r.db.Preload("Policies").Begin().Where("id = ?", id).First(&position).Error; err != nil {
-		return nil, err
+	err := r.db.Preload("Policies").Begin().Where("id = ?", id).First(&position).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, errorDomain.WrapError(errorDomain.NotFoundErr, err)
+		}
+		return nil, errorDomain.WrapError(errorDomain.GeneralDBError, err)
 	}
 	return &position, nil
 }
 
 func (r *PositionRepository) FindByFacilityID(ctx context.Context, facilityID string) ([]*positionDomain.Position, error) {
 	var positions []*positionDomain.Position
-	if err := r.db.Preload("Policies").Begin().Where("facility_id = ?", facilityID).Find(&positions).Error; err != nil {
-		return nil, err
+	err := r.db.Preload("Policies").Begin().Where("facility_id = ?", facilityID).Find(&positions).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, errorDomain.WrapError(errorDomain.GeneralDBError, err)
 	}
 	return positions, nil
 }
