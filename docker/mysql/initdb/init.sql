390a1c3de3f4f06d03e906d03c911e29f8e32d5f
Fukuemon <Fukuemony029@gmail.com>
2024-10-24 14:16:12 +0900

    🗃️ db: table構造をCognitoの仕様に合わせて変更 #17

--- docker/mysql/initdb/init.sql
+++ docker/mysql/initdb/init.sql
@@ -2,8 +2,7 @@ CREATE TABLE IF NOT EXISTS facilities (
     id VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     created_at DATETIME,
-    updated_at DATETIME,
-    deleted_at DATETIME
+    updated_at DATETIME
 );
 
 CREATE TABLE IF NOT EXISTS departments (
@@ -12,7 +11,6 @@ CREATE TABLE IF NOT EXISTS departments (
     facility_id VARCHAR(255) NOT NULL,
     created_at DATETIME,
     updated_at DATETIME,
-    deleted_at DATETIME,
     FOREIGN KEY (facility_id) REFERENCES facilities(id)
 );
 
@@ -22,7 +20,6 @@ CREATE TABLE IF NOT EXISTS teams (
     facility_id VARCHAR(255) NOT NULL,
     created_at DATETIME,
     updated_at DATETIME,
-    deleted_at DATETIME,
     FOREIGN KEY (facility_id) REFERENCES facilities(id)
 );
 
@@ -30,8 +27,7 @@ CREATE TABLE IF NOT EXISTS policies (
     id VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     created_at DATETIME,
-    updated_at DATETIME,
-    deleted_at DATETIME
+    updated_at DATETIME
 );
 
 CREATE TABLE IF NOT EXISTS positions (
@@ -40,7 +36,6 @@ CREATE TABLE IF NOT EXISTS positions (
     facility_id VARCHAR(255) NOT NULL,
     created_at DATETIME,
     updated_at DATETIME,
-    deleted_at DATETIME,
     FOREIGN KEY (facility_id) REFERENCES facilities(id)
 );
 
@@ -54,17 +49,15 @@ CREATE TABLE IF NOT EXISTS position_policies (
 
 CREATE TABLE IF NOT EXISTS users (
     id VARCHAR(255) PRIMARY KEY,
-    username VARCHAR(255) UNIQUE,
-    email VARCHAR(255) UNIQUE,
-    phone_number VARCHAR(255) UNIQUE,
-    position_id VARCHAR(255),
-    department_id VARCHAR(255),
-    team_id VARCHAR(255),
-    facility_id VARCHAR(255),
-    area_id VARCHAR(255),
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255),
+    phone_number VARCHAR(255),
+    position_id VARCHAR(255) NOT NULL,
+    department_id VARCHAR(255) NOT NULL,
+    team_id VARCHAR(255) NOT NULL,
+    facility_id VARCHAR(255) NOT NULL,
     created_at DATETIME,
     updated_at DATETIME,
-    deleted_at DATETIME,
     FOREIGN KEY (position_id) REFERENCES positions(id),
     FOREIGN KEY (department_id) REFERENCES departments(id),
     FOREIGN KEY (team_id) REFERENCES teams(id),
@@ -79,6 +72,7 @@ CREATE TABLE IF NOT EXISTS user_policies (
     FOREIGN KEY (policy_id) REFERENCES policies(id)
 );
 
+-- Insert data
 INSERT INTO facilities (id, name, created_at, updated_at)
 VALUES ('01J6SMYDSKKKNJCR2Y3242T7YX', 'テスト訪問看護ステーション', NOW(), NOW());
 
@@ -99,7 +93,6 @@ VALUES ('01J71685CQ3HA481017KTWE7B9', 'B', '01J6SMYDSKKKNJCR2Y3242T7YX', NOW(), 
 INSERT INTO teams (id, name, facility_id, created_at, updated_at)
 VALUES ('01J71685CQQF95SJ96ZGC2RBX3', 'C', '01J6SMYDSKKKNJCR2Y3242T7YX', NOW(), NOW());
 
-
 -- positions（manager, reader, member）
 INSERT INTO positions (id, name, facility_id, created_at, updated_at)
 VALUES ('01J71685CQ0ZKYEHBRADF1Q8B4', 'manager', '01J6SMYDSKKKNJCR2Y3242T7YX', NOW(), NOW());
@@ -112,8 +105,11 @@ VALUES ('01J71685CQ5GXXEE7EN2ECVQR6', 'member', '01J6SMYDSKKKNJCR2Y3242T7YX', NO
 
 -- policies（すべてのカレンダーの閲覧）
 INSERT INTO policies (id, name, created_at, updated_at)
-VALUES ('01J76AN7GHXYRSAKW8014CHRKE', 'すべてのカレンダーの閲覧', NOW(), NOW());
+VALUES ('01J76AN7GHXYRSAKW8014CHRKE', 'すべてのカレンダーの閲覧', NOW(), NOW()),
+       ('01JAYEF1X00F61QQZT51NMK4QH', 'すべてのカレンダーの編集', NOW(), NOW());
 
 -- position_policies
 INSERT INTO position_policies (position_id, policy_id)
-VALUES ('01J71685CQ0ZKYEHBRADF1Q8B4', '01J76AN7GHXYRSAKW8014CHRKE');
+VALUES ('01J71685CQ0ZKYEHBRADF1Q8B4', '01J76AN7GHXYRSAKW8014CHRKE'),
+       ('01J71685CQ0ZKYEHBRADF1Q8B4', '01JAYEF1X00F61QQZT51NMK4QH'),
+       ('01J71685CQ2JCBA6SAFTQ5MC87', '01J76AN7GHXYRSAKW8014CHRKE');
