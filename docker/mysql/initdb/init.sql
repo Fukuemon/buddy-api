ff6d19d7e45951808e594e7910edddc002fdb2fd
Fukuemon <Fukuemony029@gmail.com>
2024-10-30 09:59:50 +0900

    🗃️ db: addressとareaのテーブルと初期データを追加 #16

--- docker/mysql/initdb/init.sql
+++ docker/mysql/initdb/init.sql
@@ -47,6 +47,38 @@ CREATE TABLE IF NOT EXISTS position_policies (
     FOREIGN KEY (policy_id) REFERENCES policies(id)
 );
 
+CREATE TABLE IF NOT EXISTS addresses (
+    id VARCHAR(255) PRIMARY KEY,
+    zip_code VARCHAR(255),
+    prefecture VARCHAR(255),
+    city VARCHAR(255),
+    address_line1 VARCHAR(255),
+    address_line2 VARCHAR(255),
+    latitude DECIMAL(10, 8),
+    longitude DECIMAL(11, 8),
+    created_at DATETIME,
+    updated_at DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS areas (
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255),
+    facility_id VARCHAR(255),
+    created_at DATETIME,
+    updated_at DATETIME,
+    FOREIGN KEY (facility_id) REFERENCES facilities(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS area_addresses (
+    address_id VARCHAR(255),
+    area_id VARCHAR(255),
+    PRIMARY KEY (address_id, area_id),
+    FOREIGN KEY (address_id) REFERENCES addresses(id),
+    FOREIGN KEY (area_id) REFERENCES areas(id)
+);
+
+
 CREATE TABLE IF NOT EXISTS users (
     id VARCHAR(255) PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
@@ -113,3 +145,21 @@ INSERT INTO position_policies (position_id, policy_id)
 VALUES ('01J71685CQ0ZKYEHBRADF1Q8B4', '01J76AN7GHXYRSAKW8014CHRKE'),
        ('01J71685CQ0ZKYEHBRADF1Q8B4', '01JAYEF1X00F61QQZT51NMK4QH'),
        ('01J71685CQ2JCBA6SAFTQ5MC87', '01J76AN7GHXYRSAKW8014CHRKE');
+
+-- address
+INSERT INTO addresses (id, zip_code, prefecture, city, address_line1, address_line2, latitude, longitude, created_at, updated_at)
+VALUES ('01JBBCQ314307YD69N89XT9WBN', '000-0000', '東京都', '千代田区', '千代田1-1-1', '', 35.682839, 139.759455, NOW(), NOW()),
+        ('01JBBCPQ5SKAMZNDE9PY940ZZN', '000-0000', '東京都', '千代田区', '千代田1-1-2', '', 35.682838, 139.759454, NOW(), NOW()),
+        ('01JBBCPX6RJPK18T4B7CAJG6F3', '000-0000', '東京都', '千代田区', '千代田1-1-3', '', 35.682837, 139.759453, NOW(), NOW());
+
+-- areas
+INSERT INTO areas (id, name, facility_id, created_at, updated_at)
+VALUES ('01JBBCQ867SR9Y001CR5HD6DNJ', 'A', '01J6SMYDSKKKNJCR2Y3242T7YX', NOW(), NOW()),
+       ('01JBBCQP71J81VFQB2NRX77VDJ', 'B', '01J6SMYDSKKKNJCR2Y3242T7YX', NOW(), NOW()),
+       ('01JBBCQP71KE6PETRQQZG2658N', 'C', '01J6SMYDSKKKNJCR2Y3242T7YX', NOW(), NOW());
+
+-- area_address
+INSERT INTO area_addresses (address_id, area_id)
+VALUES ('01JBBCQ314307YD69N89XT9WBN', '01JBBCQ867SR9Y001CR5HD6DNJ'),
+       ('01JBBCPQ5SKAMZNDE9PY940ZZN', '01JBBCQ867SR9Y001CR5HD6DNJ'),
+       ('01JBBCPX6RJPK18T4B7CAJG6F3', '01JBBCQP71J81VFQB2NRX77VDJ');
