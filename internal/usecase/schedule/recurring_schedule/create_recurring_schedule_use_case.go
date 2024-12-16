919956f8339bc09df892e08218fa707d8120f344
Fukuemon <Fukuemony029@gmail.com>
2024-12-16 13:34:23 +0900

    ✨ feat: 繰り返し予定のusecaseを追加 #27

---
+++ internal/usecase/schedule/recurring_schedule/create_recurring_schedule_use_case.go
@@ -0,0 +1,146 @@
+package recurring_schedule
+
+import (
+	"api-buddy/domain/common"
+	facilityDomain "api-buddy/domain/facility"
+	recurringRuleDomain "api-buddy/domain/schedule/recurring_rule"
+	recurringScheduleDomain "api-buddy/domain/schedule/recurring_schedule"
+	scheduleTypeDomain "api-buddy/domain/schedule/schedule_type"
+	userDomain "api-buddy/domain/user"
+	visitInfoDomain "api-buddy/domain/visit_info"
+	"context"
+)
+
+type CreateRecurringScheduleUseCase struct {
+	recurringRuleRepository     recurringRuleDomain.RecurringRuleRepository
+	facilityRepository          facilityDomain.FacilityRepository
+	scheduleTypeRepository      scheduleTypeDomain.ScheduleTypeRepository
+	userRepository              userDomain.UserRepository
+	recurringScheduleRepository recurringScheduleDomain.RecurringScheduleRepository
+	visitInfoService            *visitInfoDomain.VisitInfoService
+}
+
+type CreateRecurringScheduleUseCaseInputDto struct {
+	ScheduleTypeID string
+	RecurringRule  RecurringRuleModel
+	Date           common.Date
+	StartTime      common.Time
+	EndTime        common.Time
+	StaffID        string
+	FacilityID     string
+	Title          *string
+	Description    *string
+	VisitInfo      *visitInfoDomain.VisitInfoModel
+}
+
+type RecurringRuleModel struct {
+	Frequency   recurringRuleDomain.FrequencyEnum
+	DaysOfWeek  *int
+	DayOfMonth  *int
+	WeekOfMonth *int
+	StartDate   common.Date
+	EndDate     *common.Date
+}
+
+func (uc *CreateRecurringScheduleUseCase) Run(ctx context.Context, input CreateRecurringScheduleUseCaseInputDto) (*recurringScheduleDomain.RecurringSchedule, error) {
+	recurringScheduleOptions := []recurringScheduleDomain.RecurringScheduleOption{}
+	// 予定の種類を取得
+	scheduleType, err := uc.scheduleTypeRepository.FindByID(ctx, input.ScheduleTypeID)
+	if err != nil {
+		return nil, err
+	}
+
+	// スタッフを取得
+	staff, err := uc.userRepository.FindByID(ctx, input.StaffID)
+	if err != nil {
+		return nil, err
+	}
+
+	// 施設を取得
+	facility, err := uc.facilityRepository.FindByID(ctx, input.FacilityID)
+	if err != nil {
+		return nil, err
+	}
+
+	recurringScheduleRuleOptions := []recurringRuleDomain.RecurringRuleOption{}
+
+	// 曜日が指定されている場合
+	if input.RecurringRule.DaysOfWeek != nil {
+		recurringScheduleRuleOptions = append(recurringScheduleRuleOptions, recurringRuleDomain.WithDaysOfWeek(*input.RecurringRule.DaysOfWeek))
+	}
+
+	// 月の日が指定されている場合
+	if input.RecurringRule.DayOfMonth != nil {
+		recurringScheduleRuleOptions = append(recurringScheduleRuleOptions, recurringRuleDomain.WithDayOfMonth(*input.RecurringRule.DayOfMonth))
+	}
+
+	// 週の日が指定されている場合
+	if input.RecurringRule.WeekOfMonth != nil {
+		recurringScheduleRuleOptions = append(recurringScheduleRuleOptions, recurringRuleDomain.WithWeekOfMonth(*input.RecurringRule.WeekOfMonth))
+	}
+
+	// 終了日が指定されている場合
+	if input.RecurringRule.EndDate != nil {
+		recurringScheduleRuleOptions = append(recurringScheduleRuleOptions, recurringRuleDomain.WithEndDate(*input.RecurringRule.EndDate))
+	}
+
+	// リクエストされた繰り返しルールを作成
+	recurringRule, err := recurringRuleDomain.NewRecurringRule(
+		input.RecurringRule.Frequency,
+		input.RecurringRule.StartDate,
+		recurringScheduleRuleOptions...,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// 繰り返しルールの登録
+	err = uc.recurringRuleRepository.Create(ctx, recurringRule)
+	if err != nil {
+		return nil, err
+	}
+
+	// 通常予定の場合
+	if scheduleType.Name == scheduleTypeDomain.Normal && input.Title != nil {
+		recurringScheduleOptions = append(recurringScheduleOptions, recurringScheduleDomain.WithTitle(*input.Title))
+	}
+
+	// 訪問情報がある場合
+	if scheduleType.Name == scheduleTypeDomain.Visit && input.VisitInfo != nil {
+		// 訪問情報の作成
+		visitInfo, err := uc.visitInfoService.CreateVisitInfo(ctx, *input.VisitInfo)
+		if err != nil {
+			return nil, err
+		}
+
+		recurringScheduleOptions = append(recurringScheduleOptions, recurringScheduleDomain.WithVisitInfo(visitInfo))
+	}
+
+	// 予定の説明がある場合
+	if input.Description != nil {
+		recurringScheduleOptions = append(recurringScheduleOptions, recurringScheduleDomain.WithDescription(*input.Description))
+	}
+
+	// 繰り返し予定の作成
+	recurringSchedule, err := recurringScheduleDomain.NewRecurringSchedule(
+		recurringRule,
+		scheduleType,
+		input.Date,
+		input.StartTime,
+		input.EndTime,
+		staff,
+		facility,
+		recurringScheduleOptions...,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// 繰り返し予定を登録
+	err = uc.recurringScheduleRepository.Create(ctx, recurringSchedule)
+	if err != nil {
+		return nil, err
+	}
+
+	return recurringSchedule, nil
+}
